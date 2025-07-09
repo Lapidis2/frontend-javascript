d441e00dd6aeea48c0fceb4d2c26fb4773711dba
Lapidis2 <hitlapidis2@gmail.com>
2025-07-09 11:27:49 +0200

    creating student interface

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -20,4 +20,20 @@ const studentList:Student[]=[student1,student2];
 const table=document.createElement("table")
 const headerRow=document.createElement("tr");
 const headerFirstName=document.createElement("th");
-const headerLastName=document.createElement("th");
+headerFirstName.textContent="First Name";
+const headerLocation=document.createElement("th");
+headerLocation.textContent="Location";
+headerRow.appendChild(headerFirstName);
+headerRow.appendChild(headerLocation);
+table.appendChild(headerRow);
+studentList.forEach((student)=>{
+	const row=document.createElement("tr")
+	const nameCell=document.createElement("td")
+	nameCell.textContent=student.firstName
+    const locationCell=document.createElement("td")
+	locationCell.textContent=student.location
+	row.appendChild(nameCell)
+	row.appendChild(locationCell)
+	table.appendChild(row)
+})
+document.body.appendChild(table)
